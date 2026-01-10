cf789033a50525ff61fd8844e974396709037b75
deepakmh67-maker <deepakmh67@gmail.com>
2026-01-10 00:31:56 -0500

    Update and rename my app .java to myapp.java

--- src/main/java/myapp.java
+++ src/main/java/myapp.java
@@ -1,9 +1,9 @@
-public class my App {
+public class myApp {
     public static void main(String[] args) {
         System.out.println("Build success from Jenkins!");
     }
 }
-public class my App {
+public class myApp {
     public static void main(String[] args) {
         System.out.println("Build success from Jenkins!");
     }
